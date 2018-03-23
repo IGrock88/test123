abd8792275d81fefde0bb80987b9ed44c59b9b2b
IGrock88 <igrock88@gmail.com>
2018-03-23 19:29:29 +0800

    first commit

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * User: IGrock
+ * Date: 23.03.2018
+ * Time: 19:26
+ */
+
+class hello
+{
+    public function __construct()
+    {
+        echo "hello, world";
+    }
+}
+
+
+new hello();
